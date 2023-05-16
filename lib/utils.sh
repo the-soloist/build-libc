dcfe3e98dc3a205ae0cc49466dec109d0d017cbb
Th3S <46804083+the-soloist@users.noreply.github.com>
2023-05-16 16:45:01 +0800

    fix: remove duplicate "/"

--- lib/utils.sh
+++ lib/utils.sh
@@ -84,7 +84,7 @@ function init_libc_home() {
 
     if [ -z $LIBC_HOME ]; then
         # init env while downloading source code
-        LIBC_HOME="/$DATA_HOME/$LIBC_FULL_NAME"
+        LIBC_HOME="$DATA_HOME/$LIBC_FULL_NAME"
         BUILD_LOG="/tmp/log/build-$LIBC_FULL_NAME"
     fi
 
