c931ef81b4a737bfee5f0b7268351c623c38aee9
Th3S <46804083+the-soloist@users.noreply.github.com>
2024-04-24 14:52:06 +0800

    fix: scripts

--- scripts/mutilarch-deps/arm.sh
+++ scripts/mutilarch-deps/arm.sh
@@ -4,16 +4,18 @@
 sudo apt install -y \
     linux-libc-dev-armhf-cross \
     libc6-armhf-cross libc6-dev-armhf-cross \
-    libc6-armhf-armel-cross libc6-dev-armhf-armel-cross \
     binutils-arm-linux-gnueabihf \
     gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
+
+sudo apt install libc6-armhf-armel-cross libc6-dev-armhf-armel-cross
 
 sudo apt install -y \
     linux-libc-dev-armel-cross \
     libc6-armel-cross libc6-dev-armel-cross \
-    libc6-armel-armhf-cross libc6-dev-armel-armhf-cross \
     binutils-arm-linux-gnueabi \
     gcc-arm-linux-gnueabi g++-arm-linux-gnueabi
+
+sudo apt install libc6-armel-armhf-cross libc6-dev-armel-armhf-cross
 
 sudo apt install -y \
     binutils-arm-none-eabi \
